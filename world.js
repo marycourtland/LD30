335fa5c68ecdb4876273ff82384fa563988f32f4
Mary <marycbush@gmail.com>
2014-08-23 07:44:16 -0700

    Adding world and wormhole skeleton objects

---
+++ world.js
@@ -0,0 +1,14 @@
+bz.World = function() {
+  this.resources = [
+    co2: 0,
+    water: 0
+  ];
+
+  this.outputs = [
+    temp: 0,
+    mood: 0
+  ];
+}
+
+bz.world1 = new bz.World();
+bz.world2 = new bz.World();
---
+++ wormhole.js
@@ -0,0 +1,4 @@
+bz.wormhole = {
+  this.size = 0;
+  this.flux = 0;
+}
